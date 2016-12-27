962c9e5cf0699a896d8f0c98c87a3721be3dd890
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-12-27 16:06:35 -0600

    Added in game scene

---
+++ scripts/GameRunner.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	var worldthing = preload("res://scenes/world.tscn").instance()
+	get_node("world").add_child(worldthing)
+	
+	# var guithing
+	
+	
+	pass
